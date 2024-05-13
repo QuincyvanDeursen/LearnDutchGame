dfbdd522e8f5da9ae5c3c188c2d0df8a98ae6d6a
Jelmer Verhoeven <jjp.verhoeven@student.avans.nl>
2024-05-13 18:51:25 +0200

    Added number buttons

---
+++ Assets/Scripts/ButtonSwitchScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSwitchScript : MonoBehaviour
+{
+    public GameObject letterContent;
+    public GameObject numberContent;
+    public TextMeshProUGUI buttonText;
+
+
+    public GameObject scrollView;
+
+    private bool showingLetters = true;
+    // Start is called before the first frame update
+
+    void Start() {
+        letterContent.SetActive(showingLetters);
+        numberContent.SetActive(!showingLetters);
+        buttonText.text = showingLetters ? "123" : "ABC";
+    }
+    public void Onclick() {
+        showingLetters = !showingLetters;
+
+        buttonText.text = showingLetters ? "123" : "ABC";
+        //Adjust the scroll view to show correct content
+        scrollView.GetComponent<ScrollRect>().content = showingLetters ? letterContent.GetComponent<RectTransform>() : numberContent.GetComponent<RectTransform>();
+
+        //Set the correct content to be active
+        letterContent.SetActive(showingLetters);
+        numberContent.SetActive(!showingLetters);
+    }
+}
