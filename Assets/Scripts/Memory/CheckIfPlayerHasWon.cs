d9fdca8c78db5a1f9eed666c5e6bd34028c49341
Jelmer Verhoeven <jjp.verhoeven@student.avans.nl>
2024-05-16 13:30:17 +0200

    Added difficulty to memory, linked to main menu

--- Assets/Scripts/Memory/CheckIfPlayerHasWon.cs
+++ Assets/Scripts/Memory/CheckIfPlayerHasWon.cs
@@ -8,11 +8,13 @@ public class CheckIfPlayerHasWon : MonoBehaviour
     public int score = 0;
     public int maxScore = 0;
 
+    public GameObject victoryScreen;
+
     // Update is called once per frame
 
     public void CheckPlayerScore() {
         if (score >= maxScore) {
-            Debug.Log("Player has won!");
+            victoryScreen.SetActive(true);
         }
     }
 }
